356cf1c1d2cd9832f10c54dfc366051c3af93aa7
Meraini <95693583+Meraini@users.noreply.github.com>
2026-02-08 01:26:24 +0300

    first commit

---
+++ 2. gün/intro.ts
@@ -0,0 +1,14 @@
+function addNum(num : number): number{
+    // return "hello"
+    return num + 1
+}
+
+
+
+
+function getValue(myVal: number){
+    if (myVal > 5) {
+        return true
+    }
+    //bunu yazarken şu an uyucam bundan sonrasında  da yarın devam edicem çalışmaya
+}
---
+++ intro.ts
@@ -0,0 +1,12 @@
+let greetings : string = "Hello mert";
+
+let mynum = 6;
+
+greetings.toLowerCase();
+
+// console.log(greetings);
+
+function getName(){
+    return "Mert" ;
+}
+console.log(getName());
